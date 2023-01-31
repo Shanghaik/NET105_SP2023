86da33f7f3994de1d5d690dbe8121cabe978922d
shanghaik168@gmail.com <Shanghaik1998>
2023-01-31 17:27:12 +0700

    Add project files.

---
+++ API_Core/Controllers/TestAPI.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestAPI : ControllerBase
+    {
+        // GET: api/<TestAPI>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return new List<Student>()
+            {
+                new Student{Id=Guid.NewGuid(),Name="Khoa"},
+                new Student{Id=Guid.NewGuid(),Name="Mạnh"},
+                new Student{Id=Guid.NewGuid(),Name="Tiến"},
+                new Student{Id=Guid.NewGuid(),Name="Tuấn Anh"},
+                new Student{Id=Guid.NewGuid(),Name="Phong"}
+            };
+        }
+
+        // GET api/<TestAPI>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TestAPI>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TestAPI>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TestAPI>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Student()
+        {
+
+        }
+    }
+}
---
+++ Data/Repositories/AllRepositories.cs
@@ -0,0 +1,112 @@
+﻿using Data.DbContext;
+using Data.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    internal class AllRepositories<KEntities> : IAllRepositories<KEntities> where KEntities : class
+    {
+        private readonly AppDbContext context;
+        DbSet<KEntities> Entities { get; set ; } // Tạo 1 DBSet 
+        DbSet<KEntities> IAllRepositories<KEntities>.Entities { get; set ; }
+        public AllRepositories()
+        {
+
+        }
+        public AllRepositories(AppDbContext context)
+        {
+            this.context = context;
+            //this.Entities = Entities;
+        }
+
+        public bool AddManyAsync(IEnumerable<KEntities> entity)
+        {
+            try
+            {
+                Entities.AddRange(entity); return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool AddOneAsync(KEntities entity)
+        {
+            try
+            {
+                Entities.Add(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+  
+            }
+        }
+
+        public bool DeleteManyAsync(IEnumerable<KEntities> entity)
+        {
+            try
+            {
+                Entities.RemoveRange(entity); return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteOneAsync(KEntities entity)
+        {
+            try
+            {
+                Entities.Remove(entity); return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<KEntities>> GetAllAsync()
+        {
+            return await Entities.ToListAsync(); // Lấy tất cả ra từ DBSet
+        }
+
+        public async Task<KEntities> GetOneAsync(IKey key)
+        {
+            return await Entities.FindAsync(key); // Dùng find để tìm
+        }
+
+        public bool UpdateManyAsync(IEnumerable<KEntities> entity)
+        {
+            try
+            {
+                Entities.UpdateRange(entity); return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateOneAsync(KEntities entity)
+        {
+            try
+            {
+                Entities.Update(entity); return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
